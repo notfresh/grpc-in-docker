0ba8e561ca111bf7864bcdd61cf5bc4026173dbd
notfreshmbp <1195056983@qq.com>
2022-06-02 17:52:44 +0800

    add --rm

--- make_cluster.sh
+++ make_cluster.sh
@@ -1,5 +1,5 @@
-docker run -dit --name grpc-server --network zx-net grpc-in-docker:0.2
-docker run -dit --name grpc-client1 --network zx-net grpc-in-docker:0.2 bash
-docker run -dit --name grpc-client2 --network zx-net grpc-in-docker:0.2 bash
-docker run -dit --name grpc-client3 --network zx-net grpc-in-docker:0.2 bash
+docker run -dit --rm --name grpc-server --network zx-net grpc-in-docker:0.2
+docker run -dit --rm --name grpc-client1 --network zx-net grpc-in-docker:0.2 bash
+docker run -dit --rm --name grpc-client2 --network zx-net grpc-in-docker:0.2 bash
+docker run -dit --rm --name grpc-client3 --network zx-net grpc-in-docker:0.2 bash
 
